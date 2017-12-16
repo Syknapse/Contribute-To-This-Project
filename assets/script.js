f92aa5a473acb4c47753f6406dc1ef89601ff1b6
Syknapse <syk.houdeib@gmail.com>
2017-12-16 20:14:23 +0100

    NEW feature: contributions counter
    Added JS file, logic to count number of cards, html display to show number  and a countUp function

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+// select card length
+var numberOfCards = document.getElementsByClassName("card").length;
+var displayNumber = 0;
+var contributionsDisplay = document.getElementById("contributions-number");
+var displayClass = document.getElementById("contributions-number").classList;
+var i = 1;
+
+
+function countUp() {
+    setTimeout(function() {
+        displayNumber += 1;
+        // print result to html
+        contributionsDisplay.textContent = displayNumber;
+        // increment and loop through
+        i++;
+        if (i < numberOfCards){
+            countUp();
+        }
+        if (i === numberOfCards-1){
+            displayClass.add("test");
+        }
+    }, 130)  // delay
+}
+
+countUp();
+
+// add effect
+
