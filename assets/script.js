44511784ac632ab36989e83ba98aa16629aa346e
Bhavini Harchad <bhaviniharchad@yahoo.fr>
2018-03-26 23:31:24 +0400

    Night-Mode-Feature
    Night-Mode

--- assets/script.js
+++ assets/script.js
@@ -25,5 +25,24 @@ function countUp() {
 
 countUp();
 
-// add effect
 
+// add night mode effect
+$('#toggle-box-checkbox').on('change', function(){
+  if(this.checked){
+    $('body').addClass('night');
+  }
+  else {
+    $('body').removeClass('night');
+  }
+});
+
+
+// 
+function demo(){
+    setInterval(function(){
+      $("#toggle-box-checkbox").trigger('click');
+    }, 1000);
+}
+if (document.location.pathname.indexOf('fullcpgrid')>-1){
+  demo();
+}
