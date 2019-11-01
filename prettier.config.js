25062754607e5cd893b61a0a6d95f6283cd8af05
Syk Houdeib <syk.houdeib@gmail.com>
2019-11-01 13:48:06 +0100

    Install Prettier
    Update node/npm.
    Install Husky.
    Configure pre-commit.
    Add prettier.config file

---
+++ prettier.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+    arrowParens: "avoid",
+
+    bracketSpacing: true,
+
+    endOfLine: "lf",
+
+    htmlWhitespaceSensitivity: "css",
+
+    insertPragma: false,
+
+    jsxBracketSameLine: false,
+
+    jsxSingleQuote: false,
+
+    printWidth: 120,
+
+    proseWrap: "preserve",
+
+    quoteProps: "as-needed",
+
+    requirePragma: false,
+
+    semi: false,
+
+    singleQuote: true,
+
+    tabWidth: 2,
+
+    trailingComma: "es5",
+
+    useTabs: false
+
+};
