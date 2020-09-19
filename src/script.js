10e58aebd13301a3aa35eafe2dbeb943dfdd9521
Syk Houdeib <syk.houdeib@gmail.com>
2020-09-19 20:48:38 +0200

    Move older cards to archive file and inject into DOM

---
+++ src/script.js
@@ -0,0 +1,54 @@
+// We save older cards to archive.html and inject into the DOM
+fetch('../src/archive.html')
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector('.grid').insertAdjacentHTML('afterbegin', data)
+
+    // Counter
+    const contributionsDisplay = document.getElementById('contributions-number')
+    const displayClass = document.getElementById('contributions-number').classList
+    const numberOfCards = document.getElementsByClassName('card').length
+    const numberOfContributors = numberOfCards - 1 // minus the example card
+    let displayNumber = 0
+    let i = 0
+
+    function countUp() {
+      setTimeout(function() {
+        displayNumber += 1
+        // print result to html
+        contributionsDisplay.textContent = displayNumber
+        // increment and loop through
+        i++
+        if (i < numberOfContributors) {
+          countUp()
+        }
+
+        if (i === numberOfContributors) {
+          displayClass.add('pulse')
+          displayClass.add('count-complete')
+        }
+      }, 15) // delay
+    }
+
+    countUp()
+  })
+
+// night mode feature
+$('#toggle-box-checkbox').on('change', function() {
+  if (this.checked) {
+    $('body').addClass('night')
+  } else {
+    $('body').removeClass('night')
+  }
+})
+
+function demo() {
+  setInterval(function() {
+    $('#toggle-box-checkbox').trigger('click')
+  }, 1000)
+}
+if (document.location.pathname.indexOf('fullcpgrid') > -1) {
+  demo()
+}
