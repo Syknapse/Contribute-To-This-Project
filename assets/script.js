cbfed72fbe2e89bc4f349e600612ef3b819d7c86
Syknapse <29199184+Syknapse@users.noreply.github.com>
2022-08-03 15:59:09 +0200

    Add highñighting to search results

--- assets/script.js
+++ assets/script.js
@@ -84,20 +84,43 @@ const currentYearSpan = document.getElementById('currentYear')
 const currentYear = new Date().getFullYear()
 currentYearSpan.innerText = currentYear
 
-// Search bar 
-const searchBar = document.getElementById('searchbar');
-searchBar.addEventListener('input', searchCard);
+// Search bar
+const searchBar = document.getElementById('searchbar')
+searchBar.addEventListener('input', searchCard)
+
+function clearSearchHighlights() {
+  const marks = Array.from(document.querySelectorAll('mark'))
+  if (marks.length > 0) {
+    marks.forEach(mark => {
+      mark.outerHTML = mark.innerText
+    })
+  }
+}
+
+function applyHighlightToSearchResults(value, card) {
+  const regex = new RegExp(value, 'gi')
+  const cardElements = Array.from(card.querySelectorAll('*'))
+  const matches = cardElements.filter(
+    element => element.children.length === 0 && element.textContent.toLowerCase().includes(value)
+  )
+
+  if (value && value.length > 1) {
+    matches.forEach(match => (match.innerHTML = match.textContent.replaceAll(regex, `<mark>${value}</mark>`)))
+  }
+}
 
 function searchCard() {
-  let input = searchBar.value.toLowerCase();
-  const cards = document.getElementsByClassName('card');
-    
-  for (i = 0; i < cards.length; i++) { 
-      if (!cards[i].textContent.toLowerCase().includes(input)) {
-          cards[i].style.display="none";
-      }
-      else {
-          cards[i].style.display="flex";                 
-      }
+  let input = searchBar.value.toLowerCase()
+  const cards = document.getElementsByClassName('card')
+
+  clearSearchHighlights()
+
+  for (i = 0; i < cards.length; i++) {
+    if (!cards[i].textContent.toLowerCase().includes(input)) {
+      cards[i].style.display = 'none'
+    } else {
+      cards[i].style.display = 'flex'
+      applyHighlightToSearchResults(input, cards[i])
+    }
   }
 }
