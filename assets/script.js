170d4a9107b3d8bd0dbb14fe99468c755e8b917b
Mitul Jain <mitulgoeljain0123.com>
2022-10-03 23:46:40 +0530

    Added Page Scroll Up Button

--- assets/script.js
+++ assets/script.js
@@ -124,3 +124,24 @@ function searchCard() {
     }
   }
 }
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 20) {
+      $('#toTopBtn').fadeIn();
+    } else {
+      $('#toTopBtn').fadeOut();
+    }
+  });
+
+  $('#toTopBtn').click(function() {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+  });
+});
+menu.onclick = () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
