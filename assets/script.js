b5566e2256dd53fcbe40780599af508c38165c1c
BuiTienThong <thongb1910711@student.ctu.edu.vn>
2022-11-16 15:17:43 +0700

    BuiTienThong's card
    added new card and a scroll-to-top button

--- assets/script.js
+++ assets/script.js
@@ -124,3 +124,24 @@ function searchCard() {
     }
   }
 }
+
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
