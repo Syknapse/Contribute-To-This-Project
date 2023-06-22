f2671f18b14bec659ef3876ad91a1b2f7cbe3dba
TenTraicion <tentraicion@gmail.com>
2023-06-22 23:19:19 +0600

    Fix: Check for required Cards volume

--- assets/htoj.js
+++ assets/htoj.js
@@ -5,6 +5,7 @@ const cheerio = require('cheerio')
 const htmlFile = `index.html`
 const archiveDir = 'archive'
 const script = 'assets/script.js'
+const requiredcardCount = 111
 
 // Function to extract contact details
 function extractContactDetails(contactElement) {
@@ -48,10 +49,10 @@ function extractResourceDetails(resourcesElement) {
 }
 
 // Function to save cards in a JSON file
-function saveCardsAsJSON(cards, filePath) {
+function saveCardsAsJSON(cards, filePath, num) {
   const jsonData = JSON.stringify(cards, null, 2)
   fs.writeFileSync(filePath, jsonData)
-  console.log('Saved cards as JSON')
+  console.log('\u{2714} Created archive_' + num + '.json file!')
 }
 
 // Function to delete selected cards from the index.html file
@@ -62,7 +63,7 @@ function deleteCardsFromHTML(selectedCards) {
 
   const updatedHTML = $.html()
   fs.writeFileSync(htmlFile, updatedHTML)
-  console.log('Deleted cards from HTML')
+  console.log('\u{2714} Deleted cards from' + htmlFile + 'file!')
 }
 
 // Function to update the script.js file
@@ -71,7 +72,7 @@ function updateScriptFile(script, nextNumber) {
 
   const updatedScript = scriptFile.replace(/const numberOfFiles = \d+/, `const numberOfFiles = ${nextNumber}`)
   fs.writeFileSync(script, updatedScript)
-  console.log('Updated script.js file')
+  console.log('\u{2714} Updated' + script + 'file!')
 }
 
 // Read the HTML file
@@ -82,61 +83,66 @@ const $ = cheerio.load(html)
 
 // Fetch all the cards
 const cardElements = $('.card')
+const cardCount = cardElements.length
 
-// Exclude the first 10 cards
-const selectedCards = cardElements.slice(10)
+if (cardCount < requiredcardCount) {
+  return console.log("\u{1F6AB} It's not the right time to archive the cards. Please try again later.")
+} else {
+  // Exclude the first 10 cards
+  const selectedCards = cardElements.slice(10)
 
-// Convert selected cards to JSON
-const jsonCards = convertToJSON(selectedCards)
+  // Convert selected cards to JSON
+  const jsonCards = convertToJSON(selectedCards)
 
-// Function to convert cards to JSON format
-function convertToJSON(cards) {
-  // Array to store the card objects
-  const jsonCards = []
+  // Function to convert cards to JSON format
+  function convertToJSON(cards) {
+    // Array to store the card objects
+    const jsonCards = []
 
-  // Iterate over each card
-  cards.each((index, element) => {
-    const card = {}
+    // Iterate over each card
+    cards.each((index, element) => {
+      const card = {}
 
-    // Extract name
-    card.name = $(element)
-      .find('.name')
-      .text()
-      .trim()
+      // Extract name
+      card.name = $(element)
+        .find('.name')
+        .text()
+        .trim()
 
-    // Extract contact details
-    const contactElement = $(element).find('.contact')
-    card.contacts = extractContactDetails(contactElement)
+      // Extract contact details
+      const contactElement = $(element).find('.contact')
+      card.contacts = extractContactDetails(contactElement)
 
-    // Extract about section
-    card.about = $(element)
-      .find('.about')
-      .text()
-      .trim()
+      // Extract about section
+      card.about = $(element)
+        .find('.about')
+        .text()
+        .trim()
 
-    // Extract resources
-    const resourcesElement = $(element).find('.resources')
-    card.resources = extractResourceDetails(resourcesElement)
+      // Extract resources
+      const resourcesElement = $(element).find('.resources')
+      card.resources = extractResourceDetails(resourcesElement)
 
-    // Add the card object to the array
-    jsonCards.push(card)
-  })
-  console.log('Converted cards to JSON')
-  return jsonCards
+      // Add the card object to the array
+      jsonCards.push(card)
+    })
+    console.log('\u{1F535} Converted cards to JSON')
+    return jsonCards
+  }
+
+  // Determine the next sequential number for the archive file
+  const archiveFiles = fs.readdirSync(archiveDir)
+  const nextNumber = archiveFiles.length + 1
+  const archiveFilePath = path.join(archiveDir, `archive_${nextNumber}.json`)
+
+  // Save selected cards in a JSON file
+  saveCardsAsJSON(jsonCards, archiveFilePath, nextNumber)
+
+  // Delete selected cards from index.html
+  deleteCardsFromHTML(selectedCards)
+
+  // Update the script.js file
+  updateScriptFile(script, nextNumber)
+
+  console.log('\u{1F3AF} Conversion complete!')
 }
-
-// Determine the next sequential number for the archive file
-const archiveFiles = fs.readdirSync(archiveDir)
-const nextNumber = archiveFiles.length + 1
-const archiveFilePath = path.join(archiveDir, `archive_${nextNumber}.json`)
-
-// Save selected cards in a JSON file
-saveCardsAsJSON(jsonCards, archiveFilePath)
-
-// Delete selected cards from index.html
-deleteCardsFromHTML(selectedCards)
-
-// Update the script.js file
-updateScriptFile(script, nextNumber)
-
-console.log('Conversion complete!')
