a756d31fd1226c7b892f27f3644e93c143ff1d6c
Syknapse <29199184+Syknapse@users.noreply.github.com>
2026-03-26 11:29:16 +0100

    test: intentionally unformatted file to test auto-format bot

---
+++ test-unformatted.js
@@ -0,0 +1,7 @@
+// This file is intentionally unformatted to test the auto-format bot.
+// It will be deleted after the test.
+const   x =    1
+const y="hello world"
+function   foo(a,b,c){
+return a+b+c}
+const arr = [1,2,  3,4,  5]
